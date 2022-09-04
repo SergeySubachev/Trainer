1bbdeba8bd2faac37f9dd4bd1153e657760d1d91
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2022-09-04 14:34:00 +0500

    Класс зоны

--- js/frames.js
+++ js/frames.js
@@ -106,15 +106,21 @@ class FrameClassZone extends Frame {
     Tasks = [];
 
     Init() {
-        let fz = "Технический регламент о требованиях пожарной безопасности";
-        let pue = "Правила устройства электроустановок";
         let frame = this.PrevFrame;
         while (!frame.hasOwnProperty("DateFromWorking")) {
             frame = frame.PrevFrame;
         }
-        let doc = pue;
-        if (frame.DateFromWorking >= new Date(2009, 5, 1)) doc = fz;
-        let task = new OneInManyTest("divClassZoneDocument", [ fz, pue ], doc);        
+        let doc = PUE;
+        let classZone = "В-Iг";
+        if (frame.DateFromWorking >= new Date(2009, 5, 1)) {
+            doc = FZ123;
+            classZone = "2";
+        }
+        let task = new ClassZoneDocumentTask("divClassZoneDocument", doc);        
+        this.Tasks.push(task);
+        task.Init();
+
+        task = new ClassZoneTask("divClassZone", classZone);
         this.Tasks.push(task);
         task.Init();
     }
--- js/tasks.js
+++ js/tasks.js
@@ -1,7 +1,7 @@
 ﻿class Task {
     ContainerId = "";
 
-    Init() {}
+    Init() { }
 
     IsComplete() {
         return true;
@@ -59,13 +59,27 @@ class OneInManyTest extends Task {
 
 class SubstanceCategoryTask extends OneInManyTest {
     constructor(containerId, correctOption) {
-        super(containerId, [ "IIA", "IIB", "IIC" ], correctOption);
+        super(containerId, ["IIA", "IIB", "IIC"], correctOption);
     }
 }
 
 class SubstanceGroupTask extends OneInManyTest {
     constructor(containerId, correctOption) {
-        super(containerId, [ "T1", "T2", "T3", "T4", "T5", "T6" ], correctOption);
+        super(containerId, ["T1", "T2", "T3", "T4", "T5", "T6"], correctOption);
+    }
+}
+
+const PUE = "Правила устройства электроустановок";
+const FZ123 = "Технический регламент о требованиях пожарной безопасности";
+class ClassZoneDocumentTask extends OneInManyTest {
+    constructor(containerId, correctOption) {
+        super(containerId, [PUE, FZ123], correctOption);
+    }
+}
+
+class ClassZoneTask extends OneInManyTest {
+    constructor(containerId, correctOption) {
+        super(containerId, ["П-I", "П-II", "П-IIа", "П-III", "В-I", "В-Iа", "В-Iб", "В-Iг", "В-II", "В-IIа", "0", "1", "2", "20", "21", "22"], correctOption);
     }
 }
 
