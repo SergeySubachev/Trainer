2dc3451358c48568099ae49b84a067fee0111d15
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2022-09-18 15:03:03 +0500

    Дата ввода в эксплуатацию - по центру

--- js/frameAboutStation.js
+++ js/frameAboutStation.js
@@ -5,6 +5,6 @@ class FrameAboutStation extends Frame {
         let year = 1990 + Math.round(Math.random() * 31);
         let mounth = 1 + Math.round(Math.random() * 11);
         this.DateFromWorking = new Date(year, mounth, 1);
-        document.getElementById("dateFrom").innerHTML = `Дата ввода в эксплуатацию: 01.${mounth < 10 ? "0" + mounth : mounth}.${year}.`;
+        document.getElementById("dateFrom").innerHTML = `Дата ввода в эксплуатацию:<br> 01.${mounth < 10 ? "0" + mounth : mounth}.${year}.`;
     }
 }
