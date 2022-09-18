3b177741d151fd5802bc81f5e46eeeb9e50d1711
Sergey Subachev <56560526+SergeySubachev@users.noreply.github.com>
2022-09-18 14:44:48 +0500

    Округлять, интервал поменьше #21

--- js/frameResult.js
+++ js/frameResult.js
@@ -24,11 +24,12 @@ class FrameResult extends Frame {
             userScore += res.Score;
             let p = document.createElement("p");
             p.innerText = `${res.Caption}: ${Math.round(res.Score * 100)}%.`;
+            p.style.lineHeight = 1.0;
             divDetails.appendChild(p);
         }
 
         let percent = Math.round(userScore / allScore * 100);
-        document.getElementById("resultPercent").innerText = `Результат: ${userScore} из ${allScore} (${percent}%)`;
+        document.getElementById("resultPercent").innerText = `Результат: ${Math.round(userScore * 100) / 100} из ${allScore} (${percent}%)`;
 
         super.Show();
     }
